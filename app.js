4a9b4e09dace56522c258f68970e7d07607e6dce
EC2 Default User <ec2-user@ip-172-31-16-219.ap-southeast-1.compute.internal>
2018-11-27 16:14:37 +0900

    Add post database

--- app.js
+++ app.js
@@ -2,30 +2,7 @@ var express = require("express");
 var app = express();
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
-
-var posts = [
-    {   
-        id: 0,
-        subject: "this is first post", 
-        date:"2018-11-23 00:00:00", 
-        author:"user1", 
-        content:"In leo nisi, tempus sed elit sed, tempor posuere sapien. Aenean tincidunt venenatis nibh vel eleifend. Morbi luctus eros ut fermentum aliquam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus ut ante bibendum, consequat nisi sagittis, elementum odio. Donec maximus quis metus vel rhoncus. Sed lacus purus, ultricies ut blandit sit amet, iaculis imperdiet odio. Suspendisse dapibus eu est in consequat. Suspendisse potenti. Cras elementum porttitor fringilla. Aliquam a lacinia mauris. Curabitur eget velit vehicula, venenatis neque vel, dignissim elit. Maecenas et dictum dolor. Nunc lobortis mauris vel nulla tincidunt tincidunt. Fusce bibendum pulvinar tempor."
-    },
-    {   
-        id: 1,
-        subject: "this is second post", 
-        date:"2018-11-23 00:00:00", 
-        author:"user1", 
-        content:"Proin pharetra ut leo sed posuere. Fusce rutrum suscipit augue, quis varius magna accumsan ut. Suspendisse lacinia sem risus. Duis et nulla quis quam varius suscipit quis vitae neque. Nulla id semper lacus. Vestibulum ut aliquam ante. Aliquam ut maximus purus. Nulla facilisi. Donec dictum viverra neque, non tincidunt ex mattis sit amet. Ut interdum iaculis suscipit. Sed eu suscipit arcu. Mauris eget dolor ultricies, tincidunt leo sed, pulvinar orci."
-    },
-    {   
-        id: 2,
-        subject: "this is third post", 
-        date:"2018-11-23 00:00:00", 
-        author:"user1", 
-        content:"Aenean eu tortor sit amet erat vehicula bibendum nec vitae ex. Nunc tempus quam ut elementum elementum. Nulla hendrerit vitae felis a pharetra. In fringilla metus mi, eu fermentum eros porta luctus. Aenean bibendum ante eget mi mattis iaculis. Morbi a sapien tempus, ultrices libero quis, iaculis nunc. Fusce fringilla sit amet elit id efficitur. Donec commodo, eros nec auctor blandit, sapien libero rutrum eros, at vestibulum velit sapien eget mi. Suspendisse malesuada vel diam vel tincidunt."
-    },
-];
+var mongoose = require("mongoose");
 
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
@@ -33,80 +10,93 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
+mongoose.connect("mongodb://localhost/simple_blog", { useNewUrlParser: true});
+
+var postSchema = new mongoose.Schema({
+    subject: String,
+    author: String,
+    content: String,
+    date: Date
+});
+
+var Post = mongoose.model("Post", postSchema);
+
 app.get("/", function(req, res) {
-    res.render("posts", {posts:posts});
-})
+    Post.find({}, function(err, allPosts) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("posts", {posts:allPosts});
+        }
+    })
+});
 
 app.get("/posts/new", function(req, res) {
     res.render("new");
-})
+});
 
 app.get("/posts/:id", function(req, res) {
-    posts.forEach(function(element) {
-        if (element.id == req.params.id) {
-            res.render("show", {post:element});
-            return; 
+    Post.findById(req.params.id, function(err, foundPost) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("show", {post:foundPost});
         }
     });
-})
+});
 
 app.get("/posts/:id/edit", function(req, res) {
-    posts.forEach(function(element) {
-        if (element.id == req.params.id) {
-            res.render("edit", {post:element});
-            return;
+    Post.findById(req.params.id, function(err, foundPost) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("edit", {post:foundPost});
         }
     });
-})
+});
 
 app.get("/about", function(req, res) {
     res.render("about");
-})
-
-function pad(n, width) {
-    n = n + '';
-    return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
-}
+});
 
 app.post("/posts", function(req, res) {
-    var today = new Date();
-    var date = pad(today.getFullYear(), 4) + '-' + pad(today.getMonth()+1, 2) + '-' + pad(today.getDate(), 2);
-    var time = pad(today.getHours(), 2) + ":" + pad(today.getMinutes(), 2) + ":" + pad(today.getSeconds(), 2);
-    var dateTime = date+' '+ time;
-    
-    var post = { 
-        id: posts.length+1, 
+    var newPost = { 
         subject: req.body.post.subject, 
-        date: dateTime,
+        date: Date(),
         author: "user1", 
         content: req.body.post.content
     };
    
-    posts.push(post); 
-    res.redirect("/");
-})
-
-app.put("/posts/:id", function(req, res) {
-    posts.forEach(function(element) {
-        if (element.id == req.params.id) {
-            element.subject = req.body.post.subject; 
-            element.content = req.body.post.content; 
-            res.redirect("/posts/" + req.params.id);
-            return;
+    Post.create(newPost, function(err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
         }
     });
-})
+});
+
+app.put("/posts/:id", function(req, res) {
+    req.body.post.date = Date();
+    Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, updatedPost) {
+        if (err) {
+            res.redirect("/");
+        } else {
+            res.redirect("/posts/" + req.params.id);
+        }
+    });
+});
 
 app.delete("/posts/:id", function(req, res) {
-   for (var idx=0; idx<posts.length; idx++) {
-       if (req.params.id == posts[idx].id) {
-           posts.splice(idx, 1);
-           res.redirect("/");
-           return;
-       }
-   }
-})
+    Post.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            res.redirect("/");
+        } else {
+            res.redirect("/");
+        }
+    });
+});
 
 app.listen(process.env.PORT, "0.0.0.0", function() {
     console.log("Simple Blog Server Has Started!");
-})
+});
